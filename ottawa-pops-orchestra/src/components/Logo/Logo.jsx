7c6db99fb3f6bc3f409f1b88353b6d437c7fd311
Kshamina <kshamina.ghelani@carleton.ca>
2020-06-08 22:55:02 -0400

    idk

--- ottawa-pops-orchestra/src/components/Logo/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default () => {
-  return <img src="/assets/img/OPO-Logo-Primary.png" height="100" alt=""></img>;
-};
--- ottawa-pops-orchestra/src/constants/colors.jsx
+++
@@ -1,1 +0,0 @@
-export const grey = "#757575";
